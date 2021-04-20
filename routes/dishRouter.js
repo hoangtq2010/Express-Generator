f4b539a7d3562383e7c5cc2685dcfe9f5fc2962f
hoang <36702685+hoangtq2010@users.noreply.github.com>
2021-04-21 00:46:04 +0300

    Express REST API with MongoDB and Mongoose Part 2

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');  //phân tích cú pháp vật thể
 const mongoose = require('mongoose');
 
 const Dishes = require('../models/dishes');
+const e = require('express');
 
 const dishRouter = express.Router();  //tuyen bo dishRouter la 1 bo dinh tuyen Express
 
@@ -104,6 +105,162 @@ dishRouter.route('/:dishId')   //tuyen bo diem cuoi tai 1 vi tri duy nhat
         res.statusCode = 200;
         res.setHeader('Content-Type','application/json');
         res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+
+dishRouter.route('/:dishId/comments')  
+.get( (req, res, next) =>{
+    Dishes.findById(req.params.dishId)     
+    .then((dish) => {
+        if (dish != null){
+            res.statusCode = 200;
+            res.setHeader('Content-Type','application/json');
+            res.json(dish.comments);  
+        }
+        else{
+            err = new Error('Dish ' + req.params.dishId + ' not found!');
+            res.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err)) 
+    .catch((err) => next(err));
+})
+.post((req, res, next) =>{
+    //Tao binh luan cho mon an
+    Dishes.findById(req.params.dishId)    
+    .then((dish) => {
+        if (dish != null){
+            dish.comments.push(req.body);
+            dish.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type','application/json');
+                res.json(dish);
+            } , (err) => next(err));
+        }
+        else{
+            err = new Error('Dish ' + req.params.dishId + ' not found!');
+            res.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put( (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes' + req.params.dishId + ' /comments');
+})
+.delete( (req, res, next) =>{
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        if (dish != null){
+            for(var i = (dish.comments.length - 1) ; i >= 0 ; i--){          //xoa tu ngoai vao trong
+                dish.comments.id(dish.comments[i]._id).remove();
+            }
+            dish.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type','application/json');
+                res.json(dish);
+            }, (err) => next(err));
+        }
+        else{
+            err = new Error('Dish ' + req.params.dishId + ' not found!');
+            res.status = 404;
+            return next(err);
+        }
+    },(err) => next(err))
+    .catch((err) => next(err));
+});
+
+
+
+dishRouter.route('/:dishId/comments/:commentId')   
+.get( (req, res, next) =>{
+    Dishes.findById(req.params.dishId)  
+    .then((dish) => {
+        if (dish != null && dish.comments.id(req.params.commentId) != null ){
+            res.statusCode = 200;
+            res.setHeader('Content-Type','application/json');
+            res.json(dish.comments.id(req.params.commentId));  
+        }
+        else if (dish == null){
+            err = new Error('Dish ' + req.params.dishId + ' not found!');
+            res.status = 404;
+            return next(err);
+        }
+        else{
+            err = new Error('Comment ' + req.params.commentId + ' not found!');
+            res.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post( (req, res, next) =>{
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes' + req.params.dishId + ' /comments' + req.params.commentId);
+})
+.put((req, res, next) => {
+    Dishes.findById(req.params.dishId)  
+    .then((dish) => {
+        if (dish != null && dish.comments.id(req.params.commentId) != null ){
+            /*Cap nhat binh luan mon an  
+            Neu 1 binh luan da ton tai, thì ko đc thay đổi tác giả bình luận
+            2 trường duy nhất đc cập nhật là đánh giá và nhận xét
+            */
+
+            /***Cập nhật 1 tài liệu phụ nhúng bên trong tài liệu của Mongoose***/
+            if(req.body.rating){
+                dish.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if(req.body.comment){
+                dish.comments.id(req.params.commentId).comment = req.body.comment;
+            }
+            dish.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type','application/json');
+                res.json(dish);
+            } , (err) => next(err));
+        }
+        else if (dish == null){
+            err = new Error('Dish ' + req.params.dishId + ' not found!');
+            res.status = 404;
+            return next(err);
+        }
+        else{
+            err = new Error('Comment ' + req.params.commentId + ' not found!');
+            res.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete((req, res, next) =>{
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        if (dish != null && dish.comments.id(req.params.commentId) != null ){
+            dish.comments.id(req.params.commentId).remove();
+            dish.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type','application/json');
+                res.json(dish);
+            }, (err) => next(err));
+        }
+        else if (dish == null){
+            err = new Error('Dish ' + req.params.dishId + ' not found!');
+            res.status = 404;
+            return next(err);
+        }
+        else{
+            err = new Error('Comment ' + req.params.commentId + ' not found!');
+            res.status = 404;
+            return next(err);
+        }
     }, (err) => next(err))
     .catch((err) => next(err));
 });
