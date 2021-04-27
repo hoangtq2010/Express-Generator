01d4b577f3ed5a48878566a77052d2a5045d2fa4
hoang <36702685+hoangtq2010@users.noreply.github.com>
2021-04-27 14:18:32 +0300

    Cookies

--- app.js
+++ app.js
@@ -31,36 +31,49 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('20-10-1997'));
 
 function auth(req, res, next){
-  console.log(req.headers); //req.headers: nếu thêm tiêu đề ủy quyền ta có thể nhìn thấy nó ngay tại đó
+  //console.log(req.headers); //req.headers: nếu thêm tiêu đề ủy quyền ta có thể nhìn thấy nó ngay tại đó
   //chỉ là xem những gì đang đến từ phía khác hàng
+  console.log(req.signedCookies)
+  if(!req.signedCookies){
+    var authHeader = req.headers.authorization;
+    if(!authHeader){
+      var err = new Error('You are not authenticated!');
 
-  var authHeader = req.headers.authorization;
-  if(!authHeader){
-    var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err);
+    }
 
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
-  }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
 
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
 
-  var username = auth[0];
-  var password = auth[1];
+    if(username === 'admin' && password === 'password') {
+      res.cookie('user','admin',{signed: true});
+      next(); //authorized
+    }
+    else{
+      var err = new Error('You are not authenticated!');
 
-  if(username === 'admin' && password === 'password') {
-    next();
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
   else{
-    var err = new Error('You are not authenticated!');
-
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
-  }
+    if(req.signedCookies.user === 'admin'){
+      next()
+    }
+    else{
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
+  } 
 }
 
 app.use(auth);  //mac dinh client co the truy cap
